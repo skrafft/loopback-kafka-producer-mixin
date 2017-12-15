dbcb93f674f38402e0bb85ed3d1a122ef10ba309
Sébastien Krafft <skrafft@gmail.com>
2017-12-15 09:49:58 +0100

    Added error handling and reconnect on error

--- kafka-producer.js
+++ kafka-producer.js
@@ -3,20 +3,67 @@
 var kafka = require('kafka-node');
 
 module.exports = function(Model, options) {
-   var canSend = true;
+  var canSend = true;
+  var Producer = kafka.Producer;
+  var client = null;
+  var producer = null;
+  var timeToRetryConnection = 12 * 1000; // 12 seconds
+  var reconnectInterval = null;
+
   if (!process.env.KAFKA_URL) {
     console.log('Kafka url not set, events will not be sent, please set KAFKA_URL env variable');
     canSend = false;
   }
 
   if (canSend) {
-    var Producer = kafka.Producer,
+    var _initProducer = function() {
       client = new kafka.Client(process.env.KAFKA_URL),
       producer = new Producer(client);
 
-    producer.on('ready', function() {
-      console.log('Kafka producer is ready');
-    });
+	    producer.on('ready', function() {
+	      console.log('Kafka producer is ready!!');
+	      if (reconnectInterval != null) {
+	        clearTimeout(reconnectInterval);
+	        reconnectInterval = null;
+	      }
+	    });
+
+	    producer.on('error', function(err) {
+	      producer.close();
+	      client.close();
+	      if (reconnectInterval == null) { // Multiple Error Events may fire, only set one connection retry.
+	        reconnectInterval =
+					setTimeout(function() {
+					  console.log('Reconnect is called in producer error event');
+					  _initProducer();
+					}, timeToRetryConnection);
+	      }
+	    });
+
+	    client.on('error', function(err) {
+	      producer.close();
+	      client.close(); // Comment out for client on close
+	      if (reconnectInterval == null) { // Multiple Error Events may fire, only set one connection retry.
+	        reconnectInterval =
+					setTimeout(function() {
+					  console.log('reconnect is called on client error event');
+					  _initProducer();
+					}, timeToRetryConnection);
+	      }
+	    });
+
+	    client.on('close', function(err) {
+	      producer.close();
+	      if (reconnectInterval == null) { // Multiple Error Events may fire, only set one connection retry.
+	        reconnectInterval =
+					setTimeout(function() {
+					  console.log('reconnect is called on client close');
+					  _initProducer();
+					}, timeToRetryConnection);
+	      }
+	    });
+    };
+    _initProducer();
   }
 
   Model.sendEvent = function(model, next) {
@@ -36,5 +83,4 @@ module.exports = function(Model, options) {
       }
     }
   };
-
 };
